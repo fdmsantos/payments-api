329e03d7f481d98b5845941dfab0b9a1f3c13014
Fabio Santos <fabiodanielmonteirosantos@gmail.com>
2019-03-30 20:51:56 +0000

    Add timeout to DB connection

--- infrastructure/dataBase.go
+++ infrastructure/dataBase.go
@@ -5,12 +5,13 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"os"
+	"time"
 )
 
 var db *gorm.DB
 
 func init() {
-
+	var err error
 	// Read Environment DB Variables
 	username := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASS")
@@ -18,16 +19,24 @@ func init() {
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 
+	dbUri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, username, dbName, password)
+
+	timeout := time.After(60 * time.Second)
+
+	// Waits until DB is up (Timeout 60 Seconds)
 	for {
-		// Waits from DB is UP
-		dbUri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, username, dbName, password)
-		conn, err := gorm.Open("postgres", dbUri)
-		if err != nil {
-			fmt.Print(err)
-		} else {
-			db = conn
-			break
+		select {
+		case <-timeout:
+			panic(err)
+			return
+		default:
+			db, err = gorm.Open("postgres", dbUri)
+			if err == nil {
+				// Connected With DB
+				return
+			}
 		}
+		time.Sleep(time.Second)
 	}
 
 }
